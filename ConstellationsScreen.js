a8a2ba097bcf1c6d567904b56999495ffe28db74
Artem Ufimtcev <alotxo@gmail.com>
2017-11-09 16:39:04 +0100

    Move LoginScreen to login screen file, add navigator to App.js

---
+++ ConstellationsScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class ConstellationsScreen extends React.Component {
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
